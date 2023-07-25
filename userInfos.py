94686b9226f201d955d6302e1f7e23b551b9df88
Gabriel Fernandes <gabgui2001@gmail.com>
2023-07-25 00:32:51 -0300

    Finished

--- userInfos.py
+++ userInfos.py
@@ -1,3 +1,12 @@
+from datetime import date
+
+def password():
+    data = date.today().strftime('%d-%m-%Y').split("-")
+    year = data[-1].split('20')
+    passData = data[0]+data[1]+year[-1]
+    
+    return passData
+    
 
 def inputs():
     fullnameUser = input("Digite o nome completo do usuário: ")
@@ -10,7 +19,7 @@ def inputs():
     for i in range(1, len(nameSplited)): surname = f'{surname} {nameSplited[i]}'
     surname = surname.replace(' ', '', 1)
     displayName = fullnameUser
-    accountPassword = 'Senha123' 
+    accountPassword = f'Sef@{password()}' 
     company = input("Qual empresa o usuário pertence ?: ")
     description = input("Digite a matrícula do usuário: ")
     title = input("Digite o cargo do usuário: ")
