93ce928c16d4ea47c19b1411de35d2f070447c66
Gabriel Fernandes <gabgui2001@gmail.com>
2023-07-31 10:43:17 -0300

    Acess Letter

---
+++ acessLetter.py
@@ -0,0 +1,24 @@
+from docx import Document
+
+def acessLetter(iname, iuserName, isenha, imatricula):
+    documento = Document('CartaDeAcesso.docx')
+
+
+    #print(documento.paragraphs)
+    nome = f"{iname}"
+    userName = f"{iuserName}"
+    senha = f"{isenha}"
+    matricula = f"{imatricula}"
+    email = f"{iuserName.lower()}@sefa.pa.gov.br"
+
+    for paragrafo in documento.paragraphs:
+        # print(paragrafo.text)
+        paragrafo.text = paragrafo.text.replace("NOME COMPLETO", nome)
+        paragrafo.text = paragrafo.text.replace("USUARIO DE REDE", userName)
+        paragrafo.text = paragrafo.text.replace("SENHA DE REDE", senha)
+        paragrafo.text = paragrafo.text.replace("USUARIO DE EMAIL@sefa.pa.gov.br", email)
+        paragrafo.text = paragrafo.text.replace("MATRICULA", matricula)
+
+
+    documento.save(f"Carta de Acesso - {nome}.docx")
+
--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 from userInfos import inputs 
 from userInfos import cleanInfors
+from acessLetter import acessLetter
 
 def UserAd():
     userData = inputs()
     
     script = f"PowerShell New-ADUser -Name '{userData[0]}' -UserPrincipalName '{userData[0]}@ramati.local' -GivenName  '{userData[1]}' -Surname  '{userData[2]}' -DisplayName '{userData[3]}' -AccountPassword (ConvertTo-SecureString -AsPlainText '{userData[4]}' -force) -ChangePasswordAtLogon $true -Company '{userData[5]}' -Description '{userData[6]}' -Title '{userData[7]}' -Department '{userData[8]}' -Path '{userData[9]}' -Enabled $true"
     
+    acessLetter(userData[3],userData[0], userData[4], userData[6])
     cleanInfors()
     return script
 
